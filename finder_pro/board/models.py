024930f4be72aacca5b520c8cf25813d11ac08d4
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2021-03-25 10:56:08 +0200

    Update models.py

--- finder_pro/board/models.py
+++ finder_pro/board/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from mapbox_location_field.models import LocationField, AddressAutoHiddenField
 
 # Create your models here.
+class Post(models.Model):
+    address = models.CharField(max_length=50, null=True, blank=True)
+    rating = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Location(models.Model):
+    location = LocationField(
+        map_attrs={"style": "mapbox://styles/mapbox/outdoors-v11", "marker_color": "blue", "center": (17.031645, 51.106715)})
+    create= models.DateTimeField(auto_now_add=True)
+    address = AddressAutoHiddenField()
+    
