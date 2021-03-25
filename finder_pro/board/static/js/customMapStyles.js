76d39e1846c30fe1f00a41e6a7870f70831d0d53
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2021-03-25 11:00:04 +0200

    Create customMapStyles.js

---
+++ finder_pro/board/static/js/customMapStyles.js
@@ -0,0 +1,391 @@
+function initMap() {
+let map = new google.maps.Map(document.getElementById("map"), {
+center: { lat: 42.697708, lng: 23.321867 },
+zoom: 12,
+styles: [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 0
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "33"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "61"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "42"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "22"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "31"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "62"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "-7"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "-50"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0a0914"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+],
+});
+
+    const card = document.getElementById("pac-card");
+    const input = document.getElementById("pac-input");
+    const autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo("bounds", map);
+    autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+    autocomplete.setTypes(["geocode"]);
+    autocomplete.setOptions({ strictBounds: true });
+    const infowindow = new google.maps.InfoWindow();
+    const infowindowContent = document.getElementById("infowindow-content");
+    infowindow.setContent(infowindowContent);
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29),
+        icon: 'static/images/car_marker.png'
+    });
+
+
+
+    autocomplete.addListener("place_changed", () => {
+        infowindow.close();
+        marker.setVisible(false);
+        const place = autocomplete.getPlace();
+
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(15); 
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        let address = "";
+
+        if (place.address_components) {
+            address = [
+            (place.address_components[0] &&
+                place.address_components[0].short_name) ||
+                "",
+            (place.address_components[1] &&
+                place.address_components[1].short_name) ||
+                "",
+            (place.address_components[2] &&
+                place.address_components[2].short_name) ||
+                "",
+            ].join(" ");
+        }
+        infowindowContent.children["place-icon"].src = place.icon;
+        infowindowContent.children["place-name"].textContent = place.name;
+        infowindowContent.children["place-address"].textContent = address;
+        infowindow.open(map, marker);
+
+        const currentLocation = place.geometry.location;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `find/?lat=${currentLocation.lat()}&lng=${currentLocation.lng()}`);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                const { found, name, lat, lng} = JSON.parse(xhr.responseText);
+
+                if (!found) {
+                    alert('Parking not found!');
+                    return;
+                }
+
+                const parkingLatLng = { lat, lng };
+                const parker = new google.maps.Marker({
+                    position: parkingLatLng,
+                    map
+                });
+        
+                const directionsService = new google.maps.DirectionsService();
+                const directionsDisplay = new google.maps.DirectionsRenderer();
+                directionsDisplay.setMap(map);
+        
+                let request = {
+                    origin: place.geometry.location,
+                    destination: parkingLatLng,
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+        
+                directionsService.route(request, function(response, status){
+                    if(status = 'OK'){
+                        directionsDisplay.setDirections(response);
+                    }
+                });
+        
+            } else {
+                alert('Request failed.  Returned status of ' + xhr.status);
+            }
+        };
+        xhr.send();
+    });
+/*
+    var request = {
+        location: map.center,
+        radius: 3000,
+        types: ['parking']
+    };
+
+    var places_service = new google.maps.places.PlacesService(map);
+
+    places_service.nearbySearch(request, callback);    */
+}
+
+
+/*
+function callback(results, status) {
+    if(status == google.maps.places.PlacesService.OK){
+        for (var i = 0; i < results.length; i++){
+            marker.push(createMarker(results[i]));
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: placeLoc
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+
+    return marker;
+}*/
+
