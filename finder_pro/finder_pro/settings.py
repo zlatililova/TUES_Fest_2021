00c1c6b5ba4cc0f5e090dc6b8b56193d5f410375
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2021-03-25 11:39:20 +0200

    Update settings.py

--- finder_pro/finder_pro/settings.py
+++ finder_pro/finder_pro/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'board',
-    #'users',
-    #'crispy_forms',
+    'users',
+    'crispy_forms',
     "mapbox_location_field",
     "bootstrap4",
 ]
